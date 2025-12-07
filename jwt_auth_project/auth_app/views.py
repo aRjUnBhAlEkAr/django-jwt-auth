ec971a2e61fbcf89c0afcf4b38e9acbeb06f92b0
aRjUnBhAlEkAr <arjunbhalekar37@gmail.com>
2025-12-07 16:15:41 +0530

    feat(auth): add UserRegistrationView with serializer validation and success response
     Implemented UserRegistrationView using DRF's APIView.
    
    - Added POST method to handle user registration requests.
    
    - Integrated UserRegistrationSerializer for validation and object creation.
    
    - Included proper success and error responses with appropriate HTTP status codes.
    
    - Ensured serializer raises exceptions automatically for invalid input.

--- jwt_auth_project/auth_app/views.py
+++ jwt_auth_project/auth_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# third-party packages imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# local imports
+from auth_app.serializers import UserRegistrationSerializer
 
 # Create your views here.
+class UserRegistrationView(APIView):
+    
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({"Message":{"Registration Successful!"}}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
