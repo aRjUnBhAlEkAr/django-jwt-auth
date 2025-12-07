aa8f09cb3e37aa6a11be5668d556fec96d0fb439
aRjUnBhAlEkAr <arjunbhalekar37@gmail.com>
2025-12-07 16:13:24 +0530

    feat(admin): integrate custom User model with Django UserAdmin and improve admin UI

--- jwt_auth_project/auth_app/admin.py
+++ jwt_auth_project/auth_app/admin.py
@@ -1,3 +1,45 @@
+# django default imports
 from django.contrib import admin
 
+# django required package imports
+from django.contrib.auth.admin import UserAdmin
+
+# local imports
+from auth_app.models import User
+
+# create admin model here
+class UserModelAdmin(UserAdmin):
+    list_display = ('id', 'email', 'name', 'is_admin', 'is_staff', 'is_active',)
+    
+    readonly_fields = ('created_at', 'updated_at', 'last_login')
+    
+    fieldsets = (
+        ("USER PROFILE DETAILS",
+        {
+            'fields': ('email', 'name', 'password',)
+        }),
+        ("PERMISSIONS",
+         {
+          'fields': ('is_admin', 'is_staff',)   
+         }),
+        ("Dates", 
+         {
+            "fields": ("last_login","created_at","updated_at",),
+         }),
+    )
+    
+    add_fieldset = (
+        ("UPDATE CREDIENTIALS", 
+         {
+             'fields': ("email", "name", "password", "password2")
+         }),
+    )
+    
+    filter_horizontal = ()
+    
+    search_fields = ('email',)
+    list_filter = ('email', 'is_admin',)
+    ordering = ('email', 'id',)
+        
 # Register your models here.
+admin.site.register(User, UserModelAdmin)
