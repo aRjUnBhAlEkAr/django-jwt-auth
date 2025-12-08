f7b2ce78495b1d4f13d1b645df10f594ad3c2224
aRjUnBhAlEkAr <arjunbhalekar37@gmail.com>
2025-12-08 22:54:45 +0530

    chore(auth): add login route for UserLoginView

--- jwt_auth_project/auth_app/urls.py
+++ jwt_auth_project/auth_app/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 # local imports
 from auth_app.views import (
-    UserRegistrationView
+    UserRegistrationView,
+    UserLoginView
 )
 
 urlpatterns = [
-   path('register', UserRegistrationView.as_view(), name="user-registration")
+   path('register', UserRegistrationView.as_view(), name="user-registration"),
+   path('login', UserLoginView.as_view(), name="user-login"),
 ]
